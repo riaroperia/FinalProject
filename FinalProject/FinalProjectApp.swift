aad5f5141fcfe7e1fae8ef208835d0288874561d
Ria Roperia <riaroperia06@gmail.com>
2023-08-17 12:12:28 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by roperia on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
